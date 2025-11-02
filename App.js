e1b81c391f7cc022078e823f45b2658aa6ea4b4a
Анастасия <kharlamovanastasia@gmail.com>
2025-11-02 23:14:41 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="card" style={{ width: "18rem" }}>
+        <img src="https://avatars.mds.yandex.net/i?id=a2a55b0f33135f2476b533bfa675e933_l-9289605-images-thumbs&n=13" />
+        <div className="card-body">
+          <h5 className="card-title"> Card title</h5>
+          <p className="card-text">
+            ome quick example text to build on the card title and make up the
+            bulk of the card’s content.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
